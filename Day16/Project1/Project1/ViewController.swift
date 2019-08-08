05c25cc93c35593dfeef0d88571672cd34cc51e4
admin <limicheng@126.com>
2019-08-08 17:54:16 +0800

    Project 1, 第一部分

--- Day16/Project1/Project1/ViewController.swift
+++ Day16/Project1/Project1/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                //这里加载一个图片
+                pictures.append(item)
+            }
+        }
+        print(pictures)
     }
-
-
+    //tableView 有多少条数据
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    //给tableView中cell赋值
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
 
