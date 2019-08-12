69b0a112b026f25cfa697df118a49ef12159bab9
admin <limicheng@126.com>
2019-08-12 16:47:48 +0800

    项目2,第2部分

---
+++ Day20/Project2/Project2/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by admin on 2019/8/12.
+//  Copyright © 2019 limice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    var countries = [String]()
+    var score = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        askQUestion()
+        // Do any additional setup after loading the view.
+    }
+    func askQUestion(){
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+
+}
+
