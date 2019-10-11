e905758400c4c79c4d84cf99d1b7d11635441885
admin <limicheng@126.com>
2019-10-11 09:36:05 +0800

    project10

---
+++ Day42/Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by admin on 2019/10/10.
+//  Copyright © 2019 limice. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
